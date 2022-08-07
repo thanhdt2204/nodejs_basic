d9c9686e2268744352e01f773c969c6ae8709bdc
ThanhDoan <thanhdt2204@gmail.com>
2022-08-07 16:58:14 +0700

    Add package mysql

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const chalk = require('chalk');
 const express = require('express');
 const router = require('./router.js');
+const connection = require('./database');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
+
+connection.connect(function (err) {
+    if (err) throw err;
+    var sql = "SELECT * FROM user";
+    connection.query(sql, function (err, results) {
+        if (err) throw err;
+        console.log(results);
+    })
+});
 
 app.use('/api', router);
 
